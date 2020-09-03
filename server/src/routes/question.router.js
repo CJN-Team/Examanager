67cf9a1fcfc5eb6ec056f53ecaea4d7d438ea682
JamesMorales04 <Jamesmoralesmoreno@gmail.com>
2020-09-03 15:37:35 -0500

    router change

--- server/src/routes/question.router.js
+++ server/src/routes/question.router.js
@@ -1,14 +1,99 @@
-const {getQuestions,createQuestions,deleteQuestion,getQuestion,updateQuestion} = require("../controllers/question.controller.controller");
-const { Router } = require("express");
-const router = Router();
+const express = require('express');
+const questionRouter = express.Router();
+const question = require('../models/question.model'); // question model
 
-router.route("/")
-    .get(getQuestions)
-    .post(createQuestions);
+/* Get all questions */
+questionRouter.get('/question/', (req, res, next) => {
+    question.find({} , function(err, result){
+        if(err){
+            res.status(400).send({
+                'success': false,
+                'error': err.message
+            });
+        }
+        res.status(200).send({
+            'success': true,
+            'data': result
+        });
+    });
+});
 
-router.route("/:id")
-    .delete(deleteQuestion)
-    .get(getQuestion)
-    .put(updateQuestion);
+/* Get Single question */
+questionRouter.get("/question/:question_id", (req, res, next) => {
+    question.findById(req.params.question_id, function (err, result) {
+        if(err){
+             res.status(400).send({
+               success: false,
+               error: err.message
+             });
+        }
+        res.status(200).send({
+            success: true,
+            data: result
+        });
+     });
+});
 
-module.exports = router;
+
+/* Add Single question */
+questionRouter.question("/question/", (req, res, next) => {
+  let newquestion = {
+    question:req.body.question,
+    category:req.body.category,
+    asignature:req.body.asignature,
+    thematic:req.body.thematic,
+    difficulty:req.body.difficulty,
+    answerOptions:req.body.answerOptions,
+    correctAnswer:req.body.correctAnswer,
+  };
+   question.create(newquestion, function(err, result) {
+    if(err){
+        res.status(400).send({
+          success: false,
+          error: err.message
+        });
+    }
+      res.status(201).send({
+        success: true,
+        data: result,
+        message: "question created successfully"
+      });
+  });
+});
+
+/* Edit Single question */
+questionRouter.patch("/question/:question_id", (req, res, next) => {
+  let fieldsToUpdate = req.body;
+  question.findByIdAndUpdate(req.params.question_id,{ $set: fieldsToUpdate }, { new: true },  function (err, result) {
+      if(err){
+          res.status(400).send({
+             success: false,
+            error: err.message
+            });
+      }
+      res.status(200).send({
+        success: true,
+        data: result,
+        message: "question updated successfully"
+        });
+  });
+});
+
+/* Delete Single question */
+questionRouter.delete("/question/:question_id", (req, res, next) => {
+  question.findByIdAndDelete(req.params.question_id, function(err, result){
+      if(err){
+        res.status(400).send({
+          success: false,
+          error: err.message
+        });
+      }
+    res.status(200).send({
+      success: true,
+      data: result,
+      message: "question deleted successfully"
+    });
+  });
+});
+
+module.exports = questionRouter;
--- server/src/routes/user.router.js
+++ server/src/routes/user.router.js
@@ -1,14 +1,99 @@
-const {getUsers,createUsers,deleteUsers,getUser,updateUser} = require("../controllers/user.controller");
-const { Router } = require("express");
-const router = Router();
+const express = require('express');
+const userRouter = express.Router();
+const user = require('../models/user.model'); // user model
 
-router.route("/")
-    .get(getUsers)
-    .post(createUsers);
+/* Get all users */
+userRouter.get('/user/', (req, res, next) => {
+    user.find({} , function(err, result){
+        if(err){
+            res.status(400).send({
+                'success': false,
+                'error': err.message
+            });
+        }
+        res.status(200).send({
+            'success': true,
+            'data': result
+        });
+    });
+});
 
-router.route("/:id")
-    .delete(deleteUsers)
-    .get(getUser)
-    .put(updateUser);
+/* Get Single user */
+userRouter.get("/user/:user_id", (req, res, next) => {
+    user.findById(req.params.user_id, function (err, result) {
+        if(err){
+             res.status(400).send({
+               success: false,
+               error: err.message
+             });
+        }
+        res.status(200).send({
+            success: true,
+            data: result
+        });
+     });
+});
 
-module.exports = router;
+
+/* Add Single user */
+userRouter.user("/user/", (req, res, next) => {
+  let newuser = {
+    profile:req.body.profile,
+    idType:req.body.idType,
+    name:req.body.name,
+    lastName:req.body.lastName,
+    email:req.body.email,
+    birthDate:req.body.birthDate,
+    photo:req.body.photo,
+  };
+   user.create(newuser, function(err, result) {
+    if(err){
+        res.status(400).send({
+          success: false,
+          error: err.message
+        });
+    }
+      res.status(201).send({
+        success: true,
+        data: result,
+        message: "user created successfully"
+      });
+  });
+});
+
+/* Edit Single user */
+userRouter.patch("/user/:user_id", (req, res, next) => {
+  let fieldsToUpdate = req.body;
+  user.findByIdAndUpdate(req.params.user_id,{ $set: fieldsToUpdate }, { new: true },  function (err, result) {
+      if(err){
+          res.status(400).send({
+             success: false,
+            error: err.message
+            });
+      }
+      res.status(200).send({
+        success: true,
+        data: result,
+        message: "user updated successfully"
+        });
+  });
+});
+
+/* Delete Single user */
+userRouter.delete("/user/:user_id", (req, res, next) => {
+  user.findByIdAndDelete(req.params.user_id, function(err, result){
+      if(err){
+        res.status(400).send({
+          success: false,
+          error: err.message
+        });
+      }
+    res.status(200).send({
+      success: true,
+      data: result,
+      message: "user deleted successfully"
+    });
+  });
+});
+
+module.exports = userRouter;
