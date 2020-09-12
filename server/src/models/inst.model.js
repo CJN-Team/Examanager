b19e8eb41859a1f21cf1a6bbb285ccc18ac16995
JuliannyRpoL <45438216+JuliannyRpoL@users.noreply.github.com>
2020-09-12 11:09:30 -0500

    - added institution model

---
+++ server/src/models/inst.model.js
@@ -0,0 +1,26 @@
+const {Schema, model} = require("mongoose");
+
+const instSchema=new Schema({
+    idType: {
+        type:String,
+        required:true
+    },
+    name: {
+        type:String,
+        required:true
+    },
+    instType: {
+        type:String,
+        required:true
+    },
+    address: {
+        type:String,
+        required:true
+    },
+    phoneNumber: {
+        type:String,
+        required:true
+    },
+});
+
+module.exports= model("Inst",instSchema);
